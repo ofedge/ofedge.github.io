4e8f05adc4c31168efff235eb49a15b898d7512c
ofedge <gs@lyd.app>
2025-02-21 16:16:56 +0800

    Update jquery-view.js

--- views/jquery-view.js
+++ views/jquery-view.js
@@ -8,13 +8,13 @@ const JQueryView = {
           name: 'Bootstrap Tree',
           info: 'bootstrap树结构插件',
           code: 'https://github.com/ofedge/jquery-plugin/tree/master/bootstrap-tree',
-          link: 'https://zhtw.app/jquery-plugin/bootstrap-tree/'
+          link: 'https://ofedge.github.io/jquery-plugin/bootstrap-tree/'
         },
         {
           name: 'Province And City',
           info: 'JQuery省市级联插件',
           code: 'https://github.com/ofedge/jquery-plugin/tree/master/provinceandcity',
-          link: 'https://zhtw.app/jquery-plugin/provinceandcity/'
+          link: 'https://ofedge.github.io/jquery-plugin/provinceandcity/'
         }
       ]
     }
